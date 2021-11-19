ec68a78810d671b0e1af26a53e7bb3ad7bf8a3ea
Stephan Aßmus <superstippi@gmx.de>
2021-11-19 10:06:45 +0100

    Add nav icons in global toolbar, refactoring

--- src/main/java/com/dramaqueen/club23/ui/Club23Panel.java
+++ src/main/java/com/dramaqueen/club23/ui/Club23Panel.java
@@ -2,22 +2,16 @@ package com.dramaqueen.club23.ui;
 
 import com.dramaqueen.club23.model.Document;
 import com.dramaqueen.club23.model.DocumentListener;
+import com.dramaqueen.club23.ui.jface.IconAction;
+import org.eclipse.jface.action.Action;
 import org.eclipse.swt.browser.Browser;
-import org.eclipse.swt.browser.LocationAdapter;
 import org.eclipse.swt.browser.LocationEvent;
 import org.eclipse.swt.browser.LocationListener;
-import org.eclipse.swt.events.DisposeEvent;
-import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.SWT;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringReader;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.LinkedHashMap;
@@ -27,24 +21,23 @@ public class Club23Panel extends Composite {
 
     private final Document  fDocument;
     private final Browser   fBrowser;
+    private final Action    fBackAction;
+    private final Action    fForwardAction;
 
     public Club23Panel(Composite parent, Document document) {
         super(parent, SWT.NULL);
         fDocument = document;
         final DOMPropertyUpdater domUpdater = new DOMPropertyUpdater();
         fDocument.addListener(domUpdater);
-        addDisposeListener(new DisposeListener() {
-            @Override
-            public void widgetDisposed(DisposeEvent disposeEvent) {
-                fDocument.removeListener(domUpdater);
-            }
-        });
+        addDisposeListener(disposeEvent -> fDocument.removeListener(domUpdater));
 
         fBrowser = new Browser(this, SWT.NULL);
-        fBrowser.addLocationListener(new LocationAdapter() {
+        fBrowser.addLocationListener(new LocationListener() {
             @Override
             public void changing(LocationEvent locationEvent) {
-                if (locationEvent.location.startsWith("dq://focus")) {
+                if ("about:blank".equals(locationEvent.location)) {
+                    loadBrowserContents();
+                } else if (locationEvent.location.startsWith("dq://focus")) {
                     try {
                         String query = locationEvent.location.split("\\?")[1];
                         Map<String, String> params = splitQuery(query);
@@ -59,15 +52,48 @@ public class Club23Panel extends Composite {
                     locationEvent.doit = false;
                 }
             }
+
+            @Override
+            public void changed(LocationEvent locationEvent) {
+                updateActions();
+            }
         });
 
+        fBackAction = new IconAction("Back", "go previous") {
+            @Override
+            public void run() {
+                fBrowser.back();
+            }
+        };
+
+        fForwardAction = new IconAction("Forward", "go next") {
+            @Override
+            public void run() {
+                fBrowser.forward();
+            }
+        };
+
         loadBrowserContents();
+        updateActions();
 
         setLayout(new FillLayout());
     }
 
-    private static Map<String, String> splitQuery(String query) throws UnsupportedEncodingException {
-        Map<String, String> paramMap = new LinkedHashMap<String, String>();
+    public Action getNavigateBackAction() {
+        return fBackAction;
+    }
+
+    public Action getNavigateForwardAction() {
+        return fForwardAction;
+    }
+
+    private void updateActions() {
+        fBackAction.setEnabled(fBrowser.isBackEnabled());
+        fForwardAction.setEnabled(fBrowser.isForwardEnabled());
+    }
+
+    private static Map<String, String> splitQuery(String query) {
+        Map<String, String> paramMap = new LinkedHashMap<>();
         String[] pairs = query.split("&");
         for (String pair : pairs) {
             int idx = pair.indexOf("=");
