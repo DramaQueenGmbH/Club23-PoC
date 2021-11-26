46ffd4feb9c012aa101729a682edebed0204e41c
Stephan Aßmus <superstippi@gmx.de>
2021-11-26 08:42:46 +0100

    Small fixes, no functional change

--- src/main/java/com/dramaqueen/club23/ui/club23panel/JavascriptUtils.java
+++ src/main/java/com/dramaqueen/club23/ui/club23panel/JavascriptUtils.java
@@ -1,18 +1,17 @@
 package com.dramaqueen.club23.ui.club23panel;
 
 import com.dramaqueen.club23.model.Document;
-import com.dramaqueen.club23.model.DocumentVisitor;
 
 public class JavascriptUtils {
     public static String toJavascriptObject(Document document) {
         StringBuilder builder = new StringBuilder();
         builder.append("{");
 
-        document.visitProperties((name, value) -> {
+        document.visitProperties((String name, String value) -> {
             if (builder.length() > 1)
-                builder.append(",");
+                builder.append(", ");
             builder.append(name);
-            builder.append(":'");
+            builder.append(": '");
             builder.append(value);
             builder.append("'");
         });
