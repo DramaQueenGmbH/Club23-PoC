0bbe9eb34625676a5f9e05e868c5e50ff6d393ce
Stephan Aßmus <superstippi@gmx.de>
2021-11-26 08:44:32 +0100

    Cosmetic

--- src/main/java/com/dramaqueen/club23/model/DocumentListener.java
+++ src/main/java/com/dramaqueen/club23/model/DocumentListener.java
@@ -1,5 +1,5 @@
 package com.dramaqueen.club23.model;
 
 public interface DocumentListener {
-    public void propertyChanged(String name, String value);
+    void propertyChanged(String name, String value);
 }
